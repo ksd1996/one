21ac070a6d119bd2d8b5d26acc11a00aa08d06e8
ksd1996 <kunalsinghdogra96@gmail.com>
2018-06-11 22:22:11 +0530

    New and improved parse code

--- parse.py
+++ parse.py
@@ -1,11 +1,31 @@
 import requests
 import json
+import urllib.request
 
-base_url = ("https://pub.gamezop.com/v3/games?id=")
-pub_id = input ("Enter ID: ")
-final_url = base_url + pub_id
-response_status = requests.get(final_url)
-data = response_status.json()
+choice = input ("Enter y to initiate or n to terminate: ")
 
-for game in data["games"]:
-    print (game['name']['en'])
+def fgames():
+    base_url = ("https://pub.gamezop.com/v3/games?id=")
+    pub_id = input ("Enter ID: ")
+    final_url = base_url + pub_id
+    print (final_url)
+    response_status = requests.get(final_url)
+    print (response_status)
+    response_code = response_status.status_code
+    print (response_code)
+    data = response_status.json()
+    if (response_code == 200):
+        for game in (data["games"]):
+            print (game["name"]["en"])
+    else:
+        print ("Wrong PUB ID entered")      
+        
+
+while (choice == "y"):
+    fgames()
+    
+
+
+    
+
+    
---
+++ test.py
@@ -0,0 +1,4 @@
+import requests
+import urllib
+base_url = ('https://pub.gamezop.com/v3/games?id=peSLSV')
+base_url.status_code
