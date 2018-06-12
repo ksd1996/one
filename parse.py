0c201931d8c675785f49d965f75fe925823676b2
ksd1996 <kunalsinghdogra96@gmail.com>
2018-06-12 10:50:23 +0530

    lease enter the commit message for your changes. Lines starting

--- parse.py
+++ parse.py
@@ -14,11 +14,18 @@ def fgames():
     response_code = response_status.status_code
     print (response_code)
     data = response_status.json()
+    
     if (response_code == 200):
+        print ("The request is OK.")
         for game in (data["games"]):
-            print (game["name"]["en"])
-    else:
-        print ("Wrong PUB ID entered")      
+            print (game["name"]["en"])        
+    if (response_code == 401):
+        print ("The requested page needs a username and a password.")
+    if (response_code == 501):
+        print ("The request was not completed. The server did not support the functionality required.")
+    if (response_code == 500):
+        print ("The request was not completed. The server met an unexpected condition.")                
+          
         
 
 while (choice == "y"):
