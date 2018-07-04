1510f7d4a5e8592a65bd6515f0b74ef1d8187998
ksd1996 <kunalsinghdogra96@gmail.com>
2018-07-04 16:03:21 +0530

    Final CODE

--- flaskapp.py
+++ flaskapp.py
@@ -1,32 +1,52 @@
 from flask import Flask
 from flask import json
 from flask import jsonify
+from flask import request
 import json
 import urllib.request
-import webbrowser
-import os
 import requests
+
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
+    
     return "Games: "
 
-@app.route("/PubID/<ID>")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "Invalid route."    
 
-def PubID(ID):
-    base_url = ("https://pub.gamezop.com/v3/games?id=") + ID
+@app.route("/gamenames")
+
+def gamenames():   
+    if ("id" in request.args):
+        pub_id = request.args["id"]
+    else:
+        return ("Key value does not exist. Enter id as key.")
+    
+    base_url = ("https://pub.gamezop.com/v3/games?id=") + pub_id
     response_status = requests.get(base_url)
     response_code = response_status.status_code
-    data = response_status.json()
-    gamename = []
-    for game in (data["games"]):
-        gamename.append(game["name"]["en"])
-    json_str = json.dumps(gamename)
-    return jsonify(json_str)
- 
+    
+    if (response_code == 200):
+        data = response_status.json()
+        gamename = []
+        for game in (data["games"]):
+            gamename.append(game["name"]["en"])
+        return jsonify(gamename)   
+    
+    elif (response_code == 401):
+        return jsonify("Publisher ID entered is invalid.")
+    
+    elif (response_code == 500):
+        return jsonify ("The request was not completed. The server met an unexpected condition.")
+    
+    elif (response_code == 501):
+        return jsonify ("The remote server seems to be down. Please try after some time.")
 
 if __name__ == "__main__":
     app.run(debug = True)
